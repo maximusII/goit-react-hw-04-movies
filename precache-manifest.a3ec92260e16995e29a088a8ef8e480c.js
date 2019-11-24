cff18b73802ed7c01424e12c24acbc9606f29c1f
Maksym Osadchuk <mosadchuk7@gmail.com>
2019-11-24 21:24:29 +0200

    Updates

---
+++ precache-manifest.a3ec92260e16995e29a088a8ef8e480c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb4db3c7efbd1ece9492bdbbef3d498",
+    "url": "/maximusII/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "9acf4c4cffd5e4c7a022",
+    "url": "/maximusII/goit-react-hw-04-movies/static/css/main.fddf6774.chunk.css"
+  },
+  {
+    "revision": "7896d27d9bc93a4e919e",
+    "url": "/maximusII/goit-react-hw-04-movies/static/js/2.bc127e28.chunk.js"
+  },
+  {
+    "revision": "9acf4c4cffd5e4c7a022",
+    "url": "/maximusII/goit-react-hw-04-movies/static/js/main.181ed4bb.chunk.js"
+  },
+  {
+    "revision": "67a8a4878a1029406276",
+    "url": "/maximusII/goit-react-hw-04-movies/static/js/runtime-main.0eb3818c.js"
+  }
+]);
