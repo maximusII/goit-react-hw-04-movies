f089bedbe3e193a49116cf2762fc00cb7fd0e7c8
Maksym Osadchuk <mosadchuk7@gmail.com>
2019-11-24 20:55:25 +0200

    Done + redirectToHomePage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Nav from './Nav/Nav';
 import HomePage from './HomePage/HomePage';
 import MoviesPage from './MoviesPage/MoviesPage';
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" exact component={HomePage} />
         <Route path="/movies/:movieId" component={MovieDetailsPage} />
         <Route path="/movies" component={MoviesPage} />
-        <Route component={HomePage} />
+        <Redirect from="/" to="/" />
       </Switch>
     </div>
   );
